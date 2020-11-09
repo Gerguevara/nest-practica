1b7b75386c85ebb332bb10f5256f6aab3c16a03e
Gerardo Guevara <gerardoguevara64@gmail.com>
2020-11-09 13:22:26 -0600

    installing dependencies class-validator class-transform

--- task-api/src/task/task.controller.ts
+++ task-api/src/task/task.controller.ts
@@ -8,51 +8,49 @@ import { filterTaskDto } from './dto/filterTask.dto';
 @Controller('task')
 export class TaskController {
 
-    constructor(private _taslService: TaskService) {  }
+   constructor(private _taslService: TaskService) { }
 
 
-    @Get()
-    getAlltasks(@Query() filter : filterTaskDto) :Task[]{
+   @Get()
+   getAlltasks(@Query() filter: filterTaskDto): Task[] {
 
-         if(Object.keys(filter).length){
-            console.log(filter);            
-            return this._taslService.getTaskWithFilter( filter);
-         }
-         else{
-            return this._taslService.getAllTask()
-         }
+      if (Object.keys(filter).length) {
+         console.log(filter);
+         return this._taslService.getTaskWithFilter(filter);
+      }
+      else {
+         return this._taslService.getAllTask()
+      }
+   }
 
-
-    }
-
-    @Get()
-    getOneTask(@Query('id') id : string) :Task[]{
-       return this.getOneTask(id)
-    }    
-   
+   @Get()
+   getOneTask(@Query('id') id: string): Task[] {
+      return this.getOneTask(id)
+   }
 
 
-    @Post()
-    createANewTask(@Body() task : createTaskDto) :Task{
-       return this._taslService.createNewTask(task);
-    }
 
-    // @Post()
-    // createANewTaskAlt(@Body('tittle') tittle : string, 
-    //                @Body('description') description : string) :Task{
-    //    return this._taslService.createNewTask(description ,tittle);
-    // }
+   @Post()
+   createANewTask(@Body() task: createTaskDto): Task {
+      return this._taslService.createNewTask(task);
+   }
 
-    @Delete()
-    deleteOneTask(@Query('id') id : string) :boolean{              
-       this._taslService.deleteTask(id);
-       return true
-    }
+   // @Post()
+   // createANewTaskAlt(@Body('tittle') tittle : string, 
+   //                @Body('description') description : string) :Task{
+   //    return this._taslService.createNewTask(description ,tittle);
+   // }
+
+   @Delete()
+   deleteOneTask(@Query('id') id: string): boolean {
+      this._taslService.deleteTask(id);
+      return true
+   }
 
 
-    @Patch('/:id/:status')
-    updateTask( @Param('id') id : string, @Param('status') status: TaskStatus) :boolean{
-        this._taslService.updateTask(id, status);
-        return true
-    }
+   @Patch('/:id/:status')
+   updateTask(@Param('id') id: string, @Param('status') status: TaskStatus): boolean {
+      this._taslService.updateTask(id, status);
+      return true
+   }
 }
